be2d46229ee737f328d69d11be75f6b0e7b64a80
jhdcruz <jhdcruz00@gmail.com>
2019-11-13 16:43:33 +0800

    Updated footer content box

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,12 @@ import "./styles/Footer.scss";
 export default function Footer() {
   return (
     <div className="hero-footer">
-      <div className="container has-text-centered">
-        <a
-          href="https://github.com/jhdcruz/jhdcruz.github.io"
-          className="gh-redirect animated fadeIn slower delay-1s"
-        >
-          <p>Copyright &copy; 2019 | Joshua Hero Dela Cruz</p>
-        </a>
-      </div>
+      <a
+        href="https://github.com/jhdcruz/jhdcruz.github.io"
+        className="gh-redirect"
+      >
+        Copyright &copy; 2019 | Joshua Hero Dela Cruz
+      </a>
     </div>
   );
 }
