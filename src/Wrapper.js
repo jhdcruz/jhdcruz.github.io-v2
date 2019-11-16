0ad2bc7b7f6b55b8697f2c7c65a7746e300d6ba7
Aegir Aideron <jhdcruz00@gmail.com>
2019-11-16 08:33:14 +0800

    Removed contributions component
    somehow irrelevant, might put it back again.

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -5,7 +5,6 @@ import "dependency/bulma/css/bulma.min.css";
 // * Components Import
 import Intro from "components/Introduction";
 import Projects from "components/Projects";
-import Contribution from "components/Contributions";
 import About from "components/About";
 import Contact from "components/Contact";
 
@@ -73,7 +72,6 @@ export default function Wrapper() {
         <div className="mainContent">
           <Route exact path="/" component={Intro} />
           <Route path="/projects" component={Projects} />
-          <Route path="/contributions" component={Contribution} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
         </div>
