7e824e790e79b73821781def66c3076deda22151
Aegir Aideron <jhdcruz00@gmail.com>
2019-11-18 18:47:32 +0800

    Added Profile Content | WIP

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,36 @@
 import React from "react";
 
+import ProfileImg from "images/icon.png";
+
+import {
+  Profile,
+  ProfileContent,
+  Name,
+  PersonalTitle,
+  Bio
+} from "./About.styled";
+
 export default function About() {
   return (
-    <div>
-      <h1>Joshua Hero Dela Cruz</h1>
-      <h3>A Hobbyist Front-end Web Developer</h3>
+    <div className="container">
+      {/* Profile Section */}
+      <ProfileContent>
+        <Profile src={ProfileImg} />
+        <Name>Joshua Hero Dela Cruz</Name>
+        <PersonalTitle>A Hobbyist Front-end Web Developer</PersonalTitle>
+      </ProfileContent>
+
+      <Bio>
+        Exercitation labore ut non est deserunt dolore cupidatat consectetur in
+        nostrud sit nisi est laboris. Duis ad elit do consequat. Mollit nisi ut
+        ea ea commodo est consectetur fugiat nulla ex. Sunt ad id quis nisi in
+        enim laborum ea proident duis qui commodo.
+        <br />
+        Ea occaecat quis occaecat aliqua dolore exercitation non id officia
+        culpa ex laboris id. Officia ipsum duis ad officia eiusmod ullamco
+        cupidatat veniam non ea. Quis aliqua nulla id ad elit consectetur. Eu
+        dolore ad elit officia occaecat elit.
+      </Bio>
     </div>
   );
 }
---
+++ src/components/About/About.styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+// * Profile Section
+export const ProfileContent = styled.div`
+  margin: 1.6rem 0;
+  display: block;
+  color: white;
+`;
+
+export const Profile = styled.img`
+  border-radius: 100%;
+  width: 16em;
+  height: auto;
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+export const Name = styled.h1`
+  font-size: 1.7rem;
+  display: inline-block;
+`;
+
+export const PersonalTitle = styled.h1`
+  font-size: 1.1rem;
+  color: #dbdbdb;
+  display: block;
+`;
+
+// * Bio Section
+
+export const Bio = styled.div`
+  margin: 0.5em 6em;
+  padding: 0.8em 3em;
+  color: white;
+`;
