ed5db6a5c680f10e7d93391c3251de9e7129672f
jhdcruz <jhdcruz00@gmail.com>
2019-12-10 17:52:27 +0800

    Divide about section to multiple components
    * Skills list are now fetched on `json` instead of inline

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,83 @@
 import React from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 // noinspection NpmUsedModulesInstalled
-import ProfileImg from "images/icon.png";
 import { Bio, Name, PersonalTitle, Profile, ProfileContent, Skillset, Social } from "./About.styled";
+
+import ProfileImg from "images/icon.png";
+import Skills from "./about.data";
+
+function SkillSets() {
+  // Skills Data
+  const frontEnd = Skills.FrontEnd;
+  const design = Skills.Design;
+  const services = Skills.Services;
+  const others = Skills.Others;
+
+  return <>
+    <Skillset className="columns is-gapless">
+
+      {/* Front-End */}
+      <div className="column is-one-quarter">
+        <h5><i className="fas fa-bookmark"/> Front-end</h5>
+        <ul>
+          {frontEnd.map(fe => (
+            <li><i className="fas fa-angle-right"/> {fe}</li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Design */}
+      <div className="column is-one-quarter">
+        <h5><i className="fas fa-bookmark"/> Design</h5>
+        <ul>
+          {design.map(des => (
+            <li><i className="fas fa-angle-right"/> {des}</li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Services / DevOps */}
+      <div className="column is-one-quarter">
+        <h5><i className="fas fa-bookmark"/> Services / DevOps</h5>
+        <ul>
+          {services.map(svc => (
+            <li><i className="fas fa-angle-right"/> {svc}</li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Others */}
+      <div className="column is-one-quarter">
+        <h5><i className="fas fa-bookmark"/> Others</h5>
+        <ul>
+          {others.map(oth => (
+            <li><i className="fas fa-angle-right"/> {oth}</li>
+          ))}
+        </ul>
+      </div>
+
+    </Skillset>
+  </>;
+}
+
+function SocialContacts() {
+  return <>
+    <Social>
+      <a href="https://m.me/jhdcruz">
+        <i className="fab fa-facebook-messenger"/>
+      </a>
+      <a href="https://github.com/jhdcruz">
+        <i className="fab fa-github"/>
+      </a>
+      <a href="https://stackoverflow.com/users/7976244/aegir-aideron?tab=profile">
+        <i className="fab fa-stack-overflow"/>
+      </a>
+      <a href="https://keybase.io/jhdcruz">
+        <i className="fab fa-keybase"/>
+      </a>
+    </Social>
+  </>;
+}
 
 export default function About() {
   return (
@@ -16,21 +91,7 @@ export default function About() {
         <Name>Joshua Hero Dela Cruz</Name>
         <PersonalTitle>A Front-end Web Developer & Designer</PersonalTitle>
 
-        {/* Social Media / Contact */}
-        <Social>
-          <a href="https://m.me/jhdcruz">
-            <i className="fab fa-facebook-messenger"/>
-          </a>
-          <a href="https://github.com/jhdcruz">
-            <i className="fab fa-github"/>
-          </a>
-          <a href="https://stackoverflow.com/users/7976244/aegir-aideron?tab=profile">
-            <i className="fab fa-stack-overflow"/>
-          </a>
-          <a href="https://keybase.io/jhdcruz">
-            <i className="fab fa-keybase"/>
-          </a>
-        </Social>
+        <SocialContacts/>
       </ProfileContent>
 
       {/* Biography / Introduction */}
@@ -41,53 +102,7 @@ export default function About() {
         enim laborum ea proident duis qui commodo.
       </Bio>
 
-      {/*  Skills Section */}
-      <Skillset className="columns is-gapless">
-
-        {/* Front-End */}
-        <div className="column is-one-quarter">
-          <h5><i className="fas fa-bookmark"/> Front-end</h5>
-          <ul>
-            <li><i className="fas fa-angle-right"/> JavaScript (ES5+)</li>
-            <li><i className="fas fa-angle-right"/> ReactJS 16+</li>
-            <li><i className="fas fa-angle-right"/> Electron Framework 5+</li>
-          </ul>
-        </div>
-
-        {/* Design */}
-        <div className="column is-one-quarter">
-          <h5><i className="fas fa-bookmark"/> Design</h5>
-          <ul>
-            <li><i className="fas fa-angle-right"/> Flat & Material UI</li>
-            <li><i className="fas fa-angle-right"/> Adobe XD</li>
-            <li><i className="fas fa-angle-right"/> Figma</li>
-            <li><i className="fas fa-angle-right"/> Photoshop</li>
-          </ul>
-        </div>
-
-        {/* Services / DevOps */}
-        <div className="column is-one-quarter">
-          <h5><i className="fas fa-bookmark"/> Services / DevOps</h5>
-          <ul>
-            <li><i className="fas fa-angle-right"/> TravisCI</li>
-            <li><i className="fas fa-angle-right"/> CircleCI</li>
-            <li><i className="fas fa-angle-right"/> Sentry</li>
-          </ul>
-        </div>
-
-        {/* Others */}
-        <div className="column is-one-quarter">
-          <h5><i className="fas fa-bookmark"/> Others</h5>
-          <ul>
-            <li><i className="fas fa-angle-right"/> Git VCS</li>
-            <li><i className="fas fa-angle-right"/> Linux OS</li>
-            <li><i className="fas fa-angle-right"/> Sample</li>
-            <li><i className="fas fa-angle-right"/> Sample</li>
-          </ul>
-        </div>
-
-      </Skillset>
+      <SkillSets/>
     </div>
   );
 }
-
