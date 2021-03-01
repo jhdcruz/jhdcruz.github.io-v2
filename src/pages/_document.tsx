0938abc514c39f16eab7a41e6495b8cdfd3cedab
jhdcruz <jhdcruz00@gmail.com>
2021-03-01 23:14:32 +0800

    refactor: Moved meta tags to `_document`

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,8 +6,6 @@ import Document, {
   NextScript
 } from 'next/document';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 class _Document extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     return await Document.getInitialProps(ctx);
@@ -17,9 +15,12 @@ class _Document extends Document {
     return (
       <Html lang="en">
         <Head>
-          {isProduction && (
+          <meta charSet="utf-8" />
+
+          {/* Attach when in production */}
+          {process.env.NODE_ENV === 'production' && (
             <>
-              {/*<Global site tag (gtag.js) - Google Analytics*/}
+              {/* Google Analytics v2 */}
               <script
                 async
                 src="https://www.googletagmanager.com/gtag/js?id=G-C9ZZGMKFJT"
@@ -27,17 +28,16 @@ class _Document extends Document {
               <script async src="/gtag.js" />
             </>
           )}
-          <meta charSet="utf-8" />
-          <meta name="description" content="To be filled" />
-          <meta name="copyright" content="Joshua Hero Dela Cruz" />
-          <meta
-            name="keywords"
-            content="portfolio, front-end, web, development, developer"
-          />
+
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;700&display=swap"
             rel="stylesheet"
+          />
+          <meta name="description" content="To be filled" />
+          <meta
+            name="keywords"
+            content="portfolio, front-end, web, development, developer"
           />
         </Head>
         <body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
-import Layout from '../components/Layout';
-
 const Index = () => (
-  <Layout title="Front-End Web Design to Development | Joshua Hero Dela Cruz">
-    test
-  </Layout>
+  <></>
 );
 
 export default Index;
