e485ebf9b7430d89e689c74c6c319e455ff9be44
Hero Dela Cruz <jhdcruz00@gmail.com>
2021-04-12 13:55:36 +0800

    Removed WIP notice

--- src/components/footer.js
+++ src/components/footer.js
@@ -26,7 +26,7 @@ export default function Footer() {
         href="https://github.com/jhdcruz/jhdcruz.github.io-v2"
         className="gh-redirect"
       >
-        Copyright &copy; 2019 | Joshua Hero Dela Cruz (Work in Progress)
+        Copyright &copy; 2019 | Joshua Hero Dela Cruz
       </GitHub>
     </footer>
   );
